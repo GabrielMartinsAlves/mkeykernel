04e4ce89767a3c86811eaa4a78aca3415a06103f
Gabriel Alves <gabriel.martins@sou.inteli.edu.br>
2025-09-21 18:22:27 -0300

    Implement Snake Game in Kernel
    Added a simple snake game to the kernel, allowing users to start the game by typing 'start' and control the snake using 'w', 'a', 's', and 'd' keys. The game features basic mechanics such as food spawning and collision detection. Updated README.md to reflect new game instructions and dependencies.

--- README.md
+++ README.md
@@ -1,61 +1,41 @@
 mkeykernel
 =======
 
-This is a kernel that can read the characters `a-z` and `0-9` from the keyboard and print them on screen.
+Kernel minimal com suporte a teclado e jogo da cobrinha.
 
-See the repo [mkernel](http://github.com/arjun024/mkernel) which is a minimal kernel that prints a string on the screen. mkeykernel just extends this to include keyboard support. 
+- **Start do jogo**: digitar `start`
+- **Controles**: `w` `a` `s` `d`
+- **Objetivo**: comer `@` para crescer. Fim de jogo ao bater nas bordas ou no próprio corpo.
 
+#### Dependências ####
+- NASM
+- GCC i686 (cross ou `-m32`) e binutils (`ld`) para 32-bit
+- QEMU (`qemu-system-i386`)
 
-#### Blog post ####
+No Windows, recomenda-se MSYS2/MinGW ou WSL. Em MSYS2, instale: `pacman -S nasm mingw-w64-i686-gcc mingw-w64-i686-binutils qemu`.
 
-[Kernel 201 - Let’s write a Kernel with keyboard and screen support](http://arjunsreedharan.org/post/99370248137/kernel-201-lets-write-a-kernel-with-keyboard-and)
-
-#### Build commands ####
+#### Build ####
 ```
 nasm -f elf32 kernel.asm -o kasm.o
-```
-```
-gcc -m32 -c kernel.c -o kc.o
-```
-```
+gcc -m32 -ffreestanding -fno-stack-protector -nostdlib -c kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
 ```
 
-If you get the following error message:
-```
-kc.o: In function `idt_init':
-kernel.c:(.text+0x129): undefined reference to `__stack_chk_fail'
-```
+Se surgir referência a `__stack_chk_fail`, utilize `-fno-stack-protector` como acima.
 
-compile with the `-fno-stack-protector` option:
-```
-gcc -fno-stack-protector -m32 -c kernel.c -o bin/kc.o
-```
-
-#### Test on emulator ####
+#### Rodar no QEMU ####
 ```
 qemu-system-i386 -kernel kernel
 ```
 
-#### Get to boot ####
-GRUB requires your kernel executable to be of the pattern `kernel-<version>`.
+Ao iniciar, digite `start` para começar.
 
-So, rename the kernel:
+#### Estrutura ####
+- `kernel.asm`: boot + IDT/ISR ponteiro
+- `kernel.c`: kernel, ISR do teclado e jogo Snake
+- `keyboard_map.h`: mapa de teclas
+- `link.ld`: linker script
 
-```
-mv kernel kernel-701
-```
-
-Copy it to your boot partition (assuming you are superuser):
-
-```
-cp kernel-701 /boot/kernel-701
-```
-
-Configure your grub/grub2 similar to what is given in `_grub_grub2_config` folder of [mkernel repo](http://github.com/arjun024/mkernel).
-
-Reboot.
-
-Voila!
-
-![kernel screenshot](http://31.media.tumblr.com/1afd75b433b13df613fa0c2301977893/tumblr_inline_ncy1p0kSGj1rivrqc.png "Screenshot")
+#### Observações ####
+- Modo texto VGA 80x25
+- Sem comentários no código, conforme requisito
--- kernel.c
+++ kernel.c
@@ -1,10 +1,5 @@
-/*
-* Copyright (C) 2014  Arjun Sreedharan
-* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
-*/
 #include "keyboard_map.h"
 
-/* there are 25 lines each of 80 columns; each element takes 2 bytes */
 #define LINES 25
 #define COLUMNS_IN_LINE 80
 #define BYTES_FOR_EACH_ELEMENT 2
@@ -16,17 +11,13 @@
 #define INTERRUPT_GATE 0x8e
 #define KERNEL_CODE_SEGMENT_OFFSET 0x08
 
-#define ENTER_KEY_CODE 0x1C
-
 extern unsigned char keyboard_map[128];
 extern void keyboard_handler(void);
 extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
 extern void load_idt(unsigned long *idt_ptr);
 
-/* current cursor location */
 unsigned int current_loc = 0;
-/* video memory begins at address 0xb8000 */
 char *vidptr = (char*)0xb8000;
 
 struct IDT_entry {
@@ -39,63 +30,52 @@ struct IDT_entry {
 
 struct IDT_entry IDT[IDT_SIZE];
 
+#define GRID_W 40
+#define GRID_H 20
+#define ORIGIN_X 20
+#define ORIGIN_Y 2
+
+typedef struct { int x; int y; } Point;
+
+volatile int game_started = 0;
+volatile int input_dx = 1;
+volatile int input_dy = 0;
+char start_buf[5] = {0,0,0,0,0};
+unsigned int rng_seed = 2463534242u;
+Point snake[GRID_W * GRID_H];
+int snake_length = 0;
+Point food;
+int score = 0;
 
 void idt_init(void)
 {
 	unsigned long keyboard_address;
 	unsigned long idt_address;
 	unsigned long idt_ptr[2];
-
-	/* populate IDT entry of keyboard's interrupt */
 	keyboard_address = (unsigned long)keyboard_handler;
 	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
 	IDT[0x21].selector = KERNEL_CODE_SEGMENT_OFFSET;
 	IDT[0x21].zero = 0;
 	IDT[0x21].type_attr = INTERRUPT_GATE;
 	IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
-
-	/*     Ports
-	*	 PIC1	PIC2
-	*Command 0x20	0xA0
-	*Data	 0x21	0xA1
-	*/
-
-	/* ICW1 - begin initialization */
 	write_port(0x20 , 0x11);
 	write_port(0xA0 , 0x11);
-
-	/* ICW2 - remap offset address of IDT */
-	/*
-	* In x86 protected mode, we have to remap the PICs beyond 0x20 because
-	* Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
-	*/
 	write_port(0x21 , 0x20);
 	write_port(0xA1 , 0x28);
-
-	/* ICW3 - setup cascading */
 	write_port(0x21 , 0x00);
 	write_port(0xA1 , 0x00);
-
-	/* ICW4 - environment info */
 	write_port(0x21 , 0x01);
 	write_port(0xA1 , 0x01);
-	/* Initialization finished */
-
-	/* mask interrupts */
 	write_port(0x21 , 0xff);
 	write_port(0xA1 , 0xff);
-
-	/* fill the IDT descriptor */
 	idt_address = (unsigned long)IDT ;
 	idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
 	idt_ptr[1] = idt_address >> 16 ;
-
 	load_idt(idt_ptr);
 }
 
 void kb_init(void)
 {
-	/* 0xFD is 11111101 - enables only IRQ1 (keyboard)*/
 	write_port(0x21 , 0xFD);
 }
 
@@ -121,43 +101,175 @@ void clear_screen(void)
 		vidptr[i++] = ' ';
 		vidptr[i++] = 0x07;
 	}
+	current_loc = 0;
+}
+
+void put_char_at(char c, int x, int y)
+{
+	unsigned int pos = (y * COLUMNS_IN_LINE + x) * BYTES_FOR_EACH_ELEMENT;
+	vidptr[pos] = c;
+	vidptr[pos + 1] = 0x07;
+}
+
+void draw_text_at(int x, int y, const char* s)
+{
+	int i = 0;
+	while (s[i] != '\0') {
+		put_char_at(s[i], x + i, y);
+		i++;
+	}
+}
+
+unsigned int prng(void)
+{
+	rng_seed ^= rng_seed << 13;
+	rng_seed ^= rng_seed >> 17;
+	rng_seed ^= rng_seed << 5;
+	return rng_seed;
+}
+
+int snake_contains(Point p)
+{
+	int i = 0;
+	for (i = 0; i < snake_length; i++) {
+		if (snake[i].x == p.x && snake[i].y == p.y) return 1;
+	}
+	return 0;
+}
+
+void spawn_food(void)
+{
+	Point p;
+	do {
+		p.x = (int)(prng() % GRID_W);
+		p.y = (int)(prng() % GRID_H);
+	} while (snake_contains(p));
+	food = p;
+}
+
+void render_frame(void)
+{
+	int y = 0;
+	int x = 0;
+	for (y = 0; y < GRID_H; y++) {
+		for (x = 0; x < GRID_W; x++) {
+			put_char_at(' ', ORIGIN_X + x, ORIGIN_Y + y);
+		}
+	}
+	for (y = 0; y < snake_length; y++) {
+		put_char_at('o', ORIGIN_X + snake[y].x, ORIGIN_Y + snake[y].y);
+	}
+	put_char_at('O', ORIGIN_X + snake[snake_length - 1].x, ORIGIN_Y + snake[snake_length - 1].y);
+	put_char_at('@', ORIGIN_X + food.x, ORIGIN_Y + food.y);
+	char s[32];
+	int n = score;
+	int i = 0;
+	for (i = 0; i < 32; i++) s[i] = 0;
+	s[0] = 'S'; s[1] = 'c'; s[2] = 'o'; s[3] = 'r'; s[4] = 'e'; s[5] = ':'; s[6] = ' ';
+	i = 0;
+	if (n == 0) { s[7] = '0'; s[8] = 0; }
+	else {
+		int idx = 0; char tmp[16]; int j; for (j = 0; j < 16; j++) tmp[j] = 0;
+		while (n > 0) { tmp[idx++] = (char)('0' + (n % 10)); n /= 10; }
+		for (j = 0; j < idx; j++) s[7 + j] = tmp[idx - 1 - j];
+	}
+	draw_text_at(0, 0, s);
+}
+
+void delay_ticks(int loops)
+{
+	volatile int i, j;
+	for (i = 0; i < loops; i++) {
+		for (j = 0; j < 25000; j++) { }
+	}
+}
+
+void init_game(void)
+{
+	int cx = GRID_W / 2;
+	int cy = GRID_H / 2;
+	snake_length = 3;
+	snake[0].x = cx - 2; snake[0].y = cy;
+	snake[1].x = cx - 1; snake[1].y = cy;
+	snake[2].x = cx; snake[2].y = cy;
+	input_dx = 1; input_dy = 0;
+	score = 0;
+	spawn_food();
+	render_frame();
+}
+
+void game_loop(void)
+{
+	int dx = 1;
+	int dy = 0;
+	while (1) {
+		dx = input_dx;
+		dy = input_dy;
+		Point head = snake[snake_length - 1];
+		Point next;
+		next.x = head.x + dx;
+		next.y = head.y + dy;
+		if (next.x < 0 || next.y < 0 || next.x >= GRID_W || next.y >= GRID_H) break;
+		if (snake_contains(next)) break;
+		if (next.x == food.x && next.y == food.y) {
+			snake[snake_length] = next;
+			snake_length++;
+			score++;
+			spawn_food();
+		} else {
+			int i;
+			for (i = 0; i < snake_length - 1; i++) snake[i] = snake[i + 1];
+			snake[snake_length - 1] = next;
+		}
+		render_frame();
+		delay_ticks(2);
+	}
 }
 
 void keyboard_handler_main(void)
 {
 	unsigned char status;
 	char keycode;
-
-	/* write EOI */
 	write_port(0x20, 0x20);
-
 	status = read_port(KEYBOARD_STATUS_PORT);
-	/* Lowest bit of status will be set if buffer is not empty */
 	if (status & 0x01) {
 		keycode = read_port(KEYBOARD_DATA_PORT);
-		if(keycode < 0)
-			return;
-
-		if(keycode == ENTER_KEY_CODE) {
-			kprint_newline();
-			return;
-		}
-
-		vidptr[current_loc++] = keyboard_map[(unsigned char) keycode];
-		vidptr[current_loc++] = 0x07;
+		if ((unsigned char)keycode & 0x80) return;
+		unsigned char ch = keyboard_map[(unsigned char) keycode];
+		if (!ch) return;
+		start_buf[0] = start_buf[1];
+		start_buf[1] = start_buf[2];
+		start_buf[2] = start_buf[3];
+		start_buf[3] = start_buf[4];
+		start_buf[4] = (char)ch;
+		if (start_buf[0]=='s' && start_buf[1]=='t' && start_buf[2]=='a' && start_buf[3]=='r' && start_buf[4]=='t') game_started = 1;
+		if (ch=='w') { if (!(input_dy==1)) { input_dx=0; input_dy=-1; } }
+		else if (ch=='s') { if (!(input_dy==-1)) { input_dx=0; input_dy=1; } }
+		else if (ch=='a') { if (!(input_dx==1)) { input_dx=-1; input_dy=0; } }
+		else if (ch=='d') { if (!(input_dx==-1)) { input_dx=1; input_dy=0; } }
 	}
 }
 
 void kmain(void)
 {
-	const char *str = "my first kernel with keyboard support";
 	clear_screen();
-	kprint(str);
-	kprint_newline();
-	kprint_newline();
-
+	kprint("mkeykernel snake\n\n");
+	kprint("digite start\n");
+	kprint("controles: w a s d\n\n");
 	idt_init();
 	kb_init();
-
-	while(1);
+	while (!game_started) { }
+	clear_screen();
+	init_game();
+	game_loop();
+	clear_screen();
+	kprint("game over\n");
+	kprint("score: ");
+	{
+		int n = score; char tmp[16]; int idx = 0; int i; for (i = 0; i < 16; i++) tmp[i] = 0;
+		if (n == 0) { vidptr[current_loc++] = '0'; vidptr[current_loc++] = 0x07; }
+		else { while (n > 0) { tmp[idx++] = (char)('0' + (n % 10)); n /= 10; } for (i = idx-1; i >= 0; i--) { vidptr[current_loc++] = tmp[i]; vidptr[current_loc++] = 0x07; } }
+	}
+	kprint_newline();
+	while (1) { }
 }
